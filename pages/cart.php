137726fc0cb177ea6bc964f3d9e25a7029875ed8
AdamBru <157656340+AdamBru@users.noreply.github.com>
2025-09-26 13:17:44 +0200

    commit

--- pages/cart.php
+++ pages/cart.php
@@ -12,19 +12,45 @@
 	<div class="flex-container flex-1 flex-column gap-m">
 
 		<label for="products" class="cart-header">
-			<input type="radio" name="cart-option" id="products">
+			<input type="radio" name="cart-option" id="products" checked>
 			<h3>Produkty</h3>
 		</label>
+			<div class="flex-container" id="cart-option-products">
+				Produkty <br>
+				Produkty <br>
+				Produkty <br>
+				Produkty <br>
+				Produkty <br>
+				Produkty <br>
+				Produkty <br>
+			</div>
 
 		<label for="delivery" class="cart-header">
 			<input type="radio" name="cart-option" id="delivery">
 			<h3>Dostawa</h3>
 		</label>
+			<div class="flex-container" id="cart-option-delivery">
+				Dostawa <br>
+				Dostawa <br>
+				Dostawa <br>
+				Dostawa <br>
+				Dostawa <br>
+				Dostawa <br>
+				Dostawa <br>
+			</div>
 
 		<label for="payment" class="cart-header">
 			<input type="radio" name="cart-option" id="payment">
 			<h3>Płatność</h3>
 		</label>
+			<div class="flex-container" id="cart-option-payment">
+				Płatność <br>
+				Płatność <br>
+				Płatność <br>
+				Płatność <br>
+				Płatność <br>
+				Płatność <br>
+			</div>
 
 
 	</div>
